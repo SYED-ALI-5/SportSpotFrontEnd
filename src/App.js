1b3cd2fb86ed919a093ab7a847327cdb33f8caf9
SYED-ALI-5 <syedahmadali7212@gmail.com>
2024-11-23 00:26:52 +0500

    Attach CSS File and Change App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import AuthPage from "./Components/AuthPage";
-import AuthOwner from "./Components/AuthPage_Owner";
 import Home from "./Components/Home";
+import React, { useState } from "react";
 import Footer from "./Components/Footer";
 import Ground from "./Components/Ground";
+import AuthPage_Owner from "./Components/AuthPage_Owner";
+import Reservations from "./Components/Reservations";
+import Revenue_Ground_Details from "./Components/Revenue_Ground_Details";
+import Help from "./Components/Help";
 
 function App() {
   const [authMode, setAuthMode] = useState("SignUp");
@@ -32,6 +35,10 @@ function App() {
             }
           />
           <Route path="/ground" element={<Ground />} />
+          <Route path="/authpage_owner" element={<AuthPage_Owner />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/revenue_ground_details" element={<Revenue_Ground_Details />} />
+          <Route path="/help" element={<Help />} />
         </Routes>
         <Footer />
       </Router>
